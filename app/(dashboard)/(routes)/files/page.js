9c604f4a8936dc99b4195e569555015646f9056b
Jason Felix <jasonfelix789@gmail.com>
2024-03-23 14:36:11 -0600

    removed redundant code

--- app/(dashboard)/(routes)/files/page.js
+++ app/(dashboard)/(routes)/files/page.js
@@ -5,55 +5,7 @@ import { app } from './../../../../firebaseConfig';
 import { collection, query, where, getDocs, getFirestore } from "firebase/firestore";
 import { useUser } from '@clerk/nextjs';
 
-const dummyTemp = [
-    {
-        fileName: "IMG-20190201-WA0001.jpg",
-        fileSize: 15428,
-        fileType
-            :
-            "image/jpeg",
-        fileUrl
-            :
-            "https://firebasestorage.googleapis.com/v0/b/file-shareing-23.appspot.com/o/file_share_upload%2FIMG-20190201-WA0001.jpg?alt=media&token=845b7d36-5b94-479c-a6b4-af9f7c347cc8",
-        password
-            :
-            "",
-        shortUrl
-            :
-            "http://localhost:3000/GWmAHF",
-        userEmail
-            :
-            "jaab9381@colorado.edu",
-        userName
-            :
-            "Jason Felix Abraham"
-    }, {
-        fileName
-            :
-            "IMG-20181202-WA0006.jpg",
-        fileSize
-            :
-            94545,
-        fileType
-            :
-            "image/jpeg",
-        fileUrl
-            :
-            "https://firebasestorage.googleapis.com/v0/b/file-shareing-23.appspot.com/o/file_share_upload%2FIMG-20181202-WA0006.jpg?alt=media&token=ebfa1402-50dc-48aa-8ed1-eafb65790e78",
-        password
-            :
-            "",
-        shortUrl
-            :
-            "http://localhost:3000/wLKsOz",
-        userEmail
-            :
-            "jaab9381@colorado.edu",
-        userName
-            :
-            "Jason Felix Abraham",
-    }
-]
+
 const Files = () => {
     const { user } = useUser();
     const db = getFirestore(app);
@@ -78,7 +30,6 @@ const Files = () => {
         setLoading(true);
         if (user) {
             queryFiles();
-            // setFiles(dummyTemp);
         }
         setLoading(false);
     }, [user]);
